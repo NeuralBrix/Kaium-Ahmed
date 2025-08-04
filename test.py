bb16ed41875384030bd015fde650dd170f569ea8
Md Kaium Ahmed <Md.Ahmed@haw-hamburg.de>
2025-08-04 16:59:31 +0200

    condition

---
+++ test.py
@@ -0,0 +1,10 @@
+#basic if else statement
+age = int(input("Enter your age: "))
+
+if age >= 60:
+    print(f"your age:{age} and  You have already passed your life ")
+elif age >=30:
+    print(f"your age:{age} and  You have already passed your half of the life ")
+else:
+    print(f"our age:{age} and You are still young")
+
