84643545021db0cacfff666112c3c2931e3a008e
Md Kaium Ahmed <Md.Ahmed@haw-hamburg.de>
2025-08-06 18:39:20 +0200

    06.08.2025

---
+++ .ipynb_checkpoints/test-checkpoint.py
@@ -0,0 +1,43 @@
+#basic if else statement
+''' age = int(input("Enter your age: "))
+
+if age >= 60:
+    print(f"your age:{age} and  You have already passed your life ")
+elif age >=30:
+    print(f"your age:{age} and  You have already passed your half of the life ")
+else:
+    print(f"our age:{age} and You are still young")
+
+#logical operator
+#and * or+ not! 
+print(1 and 0)
+print(1 or 0)
+#logical NOT  operator
+#true -> false     false->true
+x = True 
+print(x)
+x= not x
+print(x)
+#######################
+x = False 
+print(x)
+x= not x
+print(x)
+
+#Identity Operator
+# is , is not    eta memory location niye kaj kore
+x=10
+y=10
+print(x==y)
+print(x is y)
+print(x is not y)
+'''
+# list kind of array but list e different types er data type store kora jay
+ #list1 = [1, 2, 3,"a","kaium"]
+list1 = [1,2,3]
+list2 = list1
+list3=[1,2,3]
+print(list1 is list2)
+print(list1 is list3)
+print(list1 == list3)
+
